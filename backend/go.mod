c73ec60048f5ab001fafa3fe79461aa867a491c5
lucifer1708 <hello@sumitdhiman.in>
2025-09-26 18:29:38 +0530

    chore: update Go module dependencies, upgrade Go version to 1.24.0, and enhance token minting API with new request structure and response types

--- backend/go.mod
+++ backend/go.mod
@@ -1,9 +1,11 @@
 module rebnb
 
-go 1.21
+go 1.24.0
+
+toolchain go1.24.6
 
 require (
-	github.com/ethereum/go-ethereum v1.13.5
+	github.com/ethereum/go-ethereum v1.16.4
 	github.com/gin-gonic/gin v1.9.1
 	github.com/ipfs/go-ipfs-api v0.7.0
 	github.com/joho/godotenv v1.5.1
@@ -11,31 +13,41 @@ require (
 )
 
 require (
+	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/StackExchange/wmi v1.2.1 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
+	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/blang/semver/v4 v4.0.0 // indirect
-	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
+	github.com/consensys/gnark-crypto v0.18.0 // indirect
 	github.com/crackcomm/go-gitignore v0.0.0-20170627025303-887ab5e44cc3 // indirect
+	github.com/crate-crypto/go-eth-kzg v1.4.0 // indirect
+	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
+	github.com/deckarep/golang-set/v2 v2.6.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
+	github.com/ethereum/c-kzg-4844/v2 v2.1.3 // indirect
+	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/goccy/go-json v0.10.4 // indirect
 	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
-	github.com/holiman/uint256 v1.2.3 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/ipfs/boxo v0.12.0 // indirect
 	github.com/ipfs/go-cid v0.4.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/compress v1.15.15 // indirect
+	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/libp2p/go-buffer-pool v0.1.0 // indirect
 	github.com/libp2p/go-flow-metrics v0.1.0 // indirect
 	github.com/libp2p/go-libp2p v0.26.3 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/minio/sha256-simd v1.0.1 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -51,7 +63,11 @@ require (
 	github.com/multiformats/go-multistream v0.4.1 // indirect
 	github.com/multiformats/go-varint v0.0.7 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/supranational/blst v0.3.16-0.20250831170142-f48500c1fdbe // indirect
+	github.com/tklauser/go-sysconf v0.3.12 // indirect
+	github.com/tklauser/numcpus v0.6.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
@@ -59,13 +75,13 @@ require (
 	github.com/xdg-go/stringprep v1.0.4 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
 	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sync v0.3.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	lukechampine.com/blake3 v1.1.7 // indirect
 )
--- backend/rest/handlers/token/api.go
+++ backend/rest/handlers/token/api.go
@@ -1,7 +1,9 @@
 package token
 
 import (
+	"bytes"
 	"context"
+	"crypto/ecdsa"
 	"encoding/json"
 	"fmt"
 	"math/big"
@@ -11,16 +13,36 @@ import (
 	"rebnb/db"
 	"strconv"
 	"strings"
+	"time"
 
+	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/gin-gonic/gin"
 )
 
 // MintRequest represents the request payload for minting a property token
 type MintRequest struct {
-	To         string `json:"to" binding:"required"`
-	PropertyId string `json:"propertyId" binding:"required"`
+	PropertyName    string      `json:"property_name" binding:"required"`
+	PropertyAddress string      `json:"property_address" binding:"required"`
+	Description     string      `json:"description" binding:"required"`
+	Services        []string    `json:"services"`
+	Images          []string    `json:"images"`
+	To              string      `json:"to" binding:"required"`
+	ExternalUrl     string      `json:"external_url,omitempty"`
+	Attributes      []Attribute `json:"attributes,omitempty"`
+	// PrivateKey      string      `json:"privateKey" binding:"required"`
+	// Broadcast bool `json:"broadcast,omitempty"`
+}
+
+// Attribute represents NFT metadata attributes
+type Attribute struct {
+	TraitType   string `json:"trait_type"`
+	Value       string `json:"value"`
+	DisplayType string `json:"display_type,omitempty"`
 }
 
 // MintTransactionData represents the transaction data for the mint function
@@ -34,6 +56,56 @@ type MintTransactionData struct {
 // MintResponse represents the response for the mint endpoint
 type TxnResponse struct {
 	Msg MintTransactionData `json:"msg"`
+}
+
+// MintWithIPFSResponse represents the response for the mint with IPFS endpoint
+type MintWithIPFSResponse struct {
+	IPFSHash        string              `json:"ipfs_hash,omitempty"`
+	TokenURI        string              `json:"token_uri,omitempty"`
+	TransactionHash string              `json:"transaction_hash,omitempty"`
+	Receipt         *TransactionReceipt `json:"receipt,omitempty"`
+	PropertyId      string              `json:"property_id,omitempty"`
+	Error           string              `json:"error,omitempty"`
+}
+
+// TransactionReceipt represents a simplified transaction receipt
+type TransactionReceipt struct {
+	TransactionHash  string `json:"transaction_hash"`
+	BlockNumber      uint64 `json:"block_number"`
+	BlockHash        string `json:"block_hash"`
+	GasUsed          uint64 `json:"gas_used"`
+	Status           uint64 `json:"status"` // 1 for success, 0 for failure
+	ContractAddress  string `json:"contract_address,omitempty"`
+	TransactionIndex uint   `json:"transaction_index"`
+}
+
+// GetReceiptRequest represents the request for getting a transaction receipt
+type GetReceiptRequest struct {
+	TransactionHash string `json:"transaction_hash" binding:"required"`
+	ChainName       string `json:"chain_name,omitempty"` // defaults to "unichain"
+}
+
+// GetReceiptResponse represents the response for getting a transaction receipt
+type GetReceiptResponse struct {
+	Receipt *TransactionReceipt `json:"receipt,omitempty"`
+	Error   string              `json:"error,omitempty"`
+}
+
+// PinataResponse represents the response from Pinata API
+type PinataResponse struct {
+	IPFSHash    string `json:"IpfsHash"`
+	PinSize     int    `json:"PinSize"`
+	Timestamp   string `json:"Timestamp"`
+	IsDuplicate bool   `json:"isDuplicate"`
+}
+
+// NFTMetadata represents the NFT metadata structure
+type NFTMetadata struct {
+	Name        string      `json:"name"`
+	Description string      `json:"description"`
+	Image       string      `json:"image"`
+	ExternalURL string      `json:"external_url,omitempty"`
+	Attributes  []Attribute `json:"attributes,omitempty"`
 }
 
 // Chain represents chain data from the database
@@ -262,88 +334,88 @@ func loadPropertyABI() (*abi.ABI, error) {
 	return &contractABI, nil
 }
 
-// CreateMintMessage creates a transaction message for minting a property token
-func CreateMintMessage(c *gin.Context) {
-	var req MintRequest
+// // CreateMintMessage creates a transaction meVjssage for minting a property token
+// func CreateMintMessage(c *gin.Context) {
+// 	var req MintRequest
 
-	// Bind and validate the request
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid request payload: " + err.Error(),
-		})
-		return
-	}
+// 	// Bind and validate the request
+// 	if err := c.ShouldBindJSON(&req); err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Invalid request payload: " + err.Error(),
+// 		})
+// 		return
+// 	}
 
-	// Validate the address format
-	if !common.IsHexAddress(req.To) {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid address format",
-		})
-		return
-	}
+// 	// Validate the address format
+// 	if !common.IsHexAddress(req.To) {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Invalid address format",
+// 		})
+// 		return
+// 	}
 
-	// Validate propertyId is a valid number
-	propertyIdInt, err := strconv.ParseUint(req.PropertyId, 10, 64)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{
-			"error": "Invalid propertyId: must be a valid number",
-		})
-		return
-	}
+// 	// Validate propertyId is a valid number
+// 	propertyIdInt, err := strconv.ParseUint(req.PropertyId, 10, 64)
+// 	if err != nil {
+// 		c.JSON(http.StatusBadRequest, gin.H{
+// 			"error": "Invalid propertyId: must be a valid number",
+// 		})
+// 		return
+// 	}
 
-	// Load the contract ABI
-	contractABI, err := loadPropertyABI()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to load contract ABI: " + err.Error(),
-		})
-		return
-	}
+// 	// Load the contract ABI
+// 	contractABI, err := loadPropertyABI()
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"error": "Failed to load contract ABI: " + err.Error(),
+// 		})
+// 		return
+// 	}
 
-	// Convert parameters to proper types
-	toAddress := common.HexToAddress(req.To)
-	propertyId := new(big.Int).SetUint64(propertyIdInt)
+// 	// Convert parameters to proper types
+// 	toAddress := common.HexToAddress(req.To)
+// 	propertyId := new(big.Int).SetUint64(propertyIdInt)
 
-	// Encode the function call using ABI
-	data, err := contractABI.Pack("mint", toAddress, propertyId)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to encode function call: " + err.Error(),
-		})
-		return
-	}
+// 	// Encode the function call using ABI
+// 	data, err := contractABI.Pack("mint", toAddress, propertyId)
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"error": "Failed to encode function call: " + err.Error(),
+// 		})
+// 		return
+// 	}
 
-	chain, err := GetChain("unichain")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to get chain: " + err.Error(),
-		})
-		return
-	}
+// 	chain, err := GetChain("unichain")
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"error": "Failed to get chain: " + err.Error(),
+// 		})
+// 		return
+// 	}
 
-	contract, err := GetContract("property")
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"error": "Failed to get chain: " + err.Error(),
-		})
-		return
-	}
+// 	contract, err := GetContract("property")
+// 	if err != nil {
+// 		c.JSON(http.StatusInternalServerError, gin.H{
+// 			"error": "Failed to get chain: " + err.Error(),
+// 		})
+// 		return
+// 	}
 
-	// Create the transaction data
-	transactionData := MintTransactionData{
-		ChainId: chain.ChainID, // Base Sepolia testnet chain ID
-		To:      contract,
-		Data:    "0x" + common.Bytes2Hex(data),
-		Value:   "0x0", // No ETH value needed for minting
-	}
+// 	// Create the transaction data
+// 	transactionData := MintTransactionData{
+// 		ChainId: chain.ChainID, // Base Sepolia testnet chain ID
+// 		To:      contract,
+// 		Data:    "0x" + common.Bytes2Hex(data),
+// 		Value:   "0x0", // No ETH value needed for minting
+// 	}
 
-	// Create the response
-	response := TxnResponse{
-		Msg: transactionData,
-	}
+// 	// Create the response
+// 	response := TxnResponse{
+// 		Msg: transactionData,
+// 	}
 
-	c.JSON(http.StatusOK, response)
-}
+// 	c.JSON(http.StatusOK, response)
+// }
 
 // GetChain fetches chain data for a specific chain from MongoDB
 func GetChain(chainName string) (*Chain, error) {
@@ -384,4 +456,294 @@ func GetContract(contractType string) (string, error) {
 	}
 
 	return contractDoc.ContractAddress, nil
+}
+
+// uploadToIPFS uploads JSON metadata to Pinata IPFS
+func uploadToIPFS(metadata NFTMetadata, pinataJWT string) (*PinataResponse, error) {
+
+	// Create the request payload for Pinata
+	pinataPayload := map[string]interface{}{
+		"pinataOptions": map[string]interface{}{
+			"cidVersion": 1,
+		},
+		"pinataMetadata": map[string]interface{}{
+			"name": fmt.Sprintf("%s.json", metadata.Name),
+		},
+		"pinataContent": metadata,
+	}
+
+	url := "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+
+	// Convert metadata to JSON
+	jsonData, err := json.Marshal(pinataPayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal metadata: %w", err)
+	}
+
+	// Create the request
+	req, err := http.NewRequestWithContext(context.Background(), "POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// Set headers
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("pinata_api_key", os.Getenv("PINATA_API_KEY"))
+	req.Header.Set("pinata_secret_api_key", os.Getenv("PINATA_SECRET_API_KEY"))
+	fmt.Println(req.Header)
+
+	// Make the request
+	client := &http.Client{Timeout: 30 * time.Second}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to make request: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("pinata API returned status %d", resp.StatusCode)
+	}
+
+	// Parse response
+	var pinataResp PinataResponse
+	if err := json.NewDecoder(resp.Body).Decode(&pinataResp); err != nil {
+		return nil, fmt.Errorf("failed to decode response: %v", err)
+	}
+
+	return &pinataResp, nil
+}
+
+// broadcastTransaction broadcasts a transaction to the blockchain
+func broadcastTransaction(rpcURL string, privateKeyHex string, to common.Address, data []byte, chainID *big.Int) (string, error) {
+	// Connect to the blockchain
+	client, err := ethclient.Dial(rpcURL)
+	if err != nil {
+		return "", fmt.Errorf("failed to connect to blockchain: %v", err)
+	}
+	defer client.Close()
+
+	// Parse private key
+	privateKey, err := crypto.HexToECDSA(privateKeyHex)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse private key: %v", err)
+	}
+
+	// Get public key and address
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		return "", fmt.Errorf("error casting public key to ECDSA")
+	}
+
+	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
+
+	// Get nonce
+	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+	if err != nil {
+		return "", fmt.Errorf("failed to get nonce: %v", err)
+	}
+
+	// Get gas price
+	gasPrice, err := client.SuggestGasPrice(context.Background())
+	if err != nil {
+		return "", fmt.Errorf("failed to get gas price: %v", err)
+	}
+
+	// Estimate gas limit
+	gasLimit, err := client.EstimateGas(context.Background(), ethereum.CallMsg{
+		From: fromAddress,
+		To:   &to,
+		Data: data,
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to estimate gas: %v", err)
+	}
+
+	// Create transaction
+	tx := types.NewTransaction(nonce, to, big.NewInt(0), gasLimit, gasPrice, data)
+
+	// Sign transaction
+	signedTx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), privateKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to sign transaction: %v", err)
+	}
+
+	// Send transaction
+	err = client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		return "", fmt.Errorf("failed to send transaction: %v", err)
+	}
+
+	return signedTx.Hash().Hex(), nil
+}
+
+// CreateMintWithIPFS creates a mint transaction with IPFS metadata upload and optional broadcasting
+func CreateMintMessage(c *gin.Context) {
+	var req MintRequest
+
+	// Bind and validate the request
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, MintWithIPFSResponse{
+			Error: "Invalid request payload: " + err.Error(),
+		})
+		return
+	}
+
+	// Validate the address format
+	if !common.IsHexAddress(req.To) {
+		c.JSON(http.StatusBadRequest, MintWithIPFSResponse{
+			Error: "Invalid address format",
+		})
+		return
+	}
+	ppId := fmt.Sprintf("%08d", time.Now().UnixNano()%1e8)
+
+	// Validate propertyId is a valid number
+	propertyIdInt, err := strconv.ParseUint(ppId, 10, 64)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, MintWithIPFSResponse{
+			Error: "Invalid propertyId: must be a valid number",
+		})
+		return
+	}
+
+	// Get Pinata JWT from environment
+	pinataJWT := os.Getenv("PINATA_JWT")
+	if pinataJWT == "" {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Pinata JWT not configured",
+		})
+		return
+	}
+
+	// Create NFT metadata
+	metadata := NFTMetadata{
+		Name:        req.PropertyName,
+		Description: req.Description,
+		ExternalURL: req.ExternalUrl,
+		Attributes:  req.Attributes,
+	}
+
+	// Set image URL if provided
+	if len(req.Images) > 0 {
+		metadata.Image = req.Images[0]
+	}
+
+	// Add default attributes if none provided
+	if len(metadata.Attributes) == 0 {
+		metadata.Attributes = []Attribute{
+			{
+				TraitType: "property_id",
+				Value:     ppId,
+			},
+			{
+				TraitType:   "date_of_mint",
+				Value:       fmt.Sprintf("%d", time.Now().Unix()),
+				DisplayType: "date",
+			},
+			{
+				TraitType: "property_address",
+				Value:     req.PropertyAddress,
+			},
+		}
+	}
+
+	// Upload to IPFS via Pinata
+	pinataResp, err := uploadToIPFS(metadata, pinataJWT)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Failed to upload to IPFS: " + err.Error(),
+		})
+		return
+	}
+
+	// Create token URI
+	tokenURI := fmt.Sprintf("https://pink-improved-swift-480.mypinata.cloud/ipfs/%s", pinataResp.IPFSHash)
+
+	// Load the contract ABI
+	contractABI, err := loadPropertyABI()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Failed to load contract ABI: " + err.Error(),
+		})
+		return
+	}
+
+	// Convert parameters to proper types
+	toAddress := common.HexToAddress(req.To)
+	propertyId := new(big.Int).SetUint64(propertyIdInt)
+
+	// Encode the function call using ABI (assuming mint function takes to, propertyId, tokenURI)
+	data, err := contractABI.Pack("mint", toAddress, propertyId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Failed to encode function call: " + err.Error(),
+		})
+		return
+	}
+
+	// Get chain and contract configuration
+	chain, err := GetChain("unichain")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Failed to get chain: " + err.Error(),
+		})
+		return
+	}
+
+	contractAddress, err := GetContract("property")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+			Error: "Failed to get contract: " + err.Error(),
+		})
+		return
+	}
+
+	// Create the transaction data
+
+	response := MintWithIPFSResponse{
+		IPFSHash:   pinataResp.IPFSHash,
+		TokenURI:   tokenURI,
+		PropertyId: ppId,
+	}
+
+	privateKey := os.Getenv(
+		"PRIVATE_KEY",
+	)
+
+	// If broadcast is requested, send the transaction
+	if privateKey != "" {
+		// Remove 0x prefix if present
+		privateKey := strings.TrimPrefix(privateKey, "0x")
+
+		// Parse chain ID
+		chainID, success := new(big.Int).SetString(chain.ChainID, 0)
+		if !success {
+			c.JSON(http.StatusInternalServerError, MintWithIPFSResponse{
+				Error: "Invalid chain ID format",
+			})
+			return
+		}
+
+		// Add debugging information
+		fmt.Printf("DEBUG: Contract address: %s\n", contractAddress)
+		fmt.Printf("DEBUG: Chain ID: %s\n", chain.ChainID)
+		fmt.Printf("DEBUG: RPC URL: %s\n", chain.RPC)
+
+		// Broadcast transaction
+		txHash, err := broadcastTransaction(
+			chain.RPC,
+			privateKey,
+			common.HexToAddress(contractAddress),
+			data,
+			chainID,
+		)
+		if err != nil {
+			response.Error = "Failed to broadcast transaction: " + err.Error()
+			fmt.Printf("DEBUG: Transaction error: %s\n", err.Error())
+		}
+		response.TransactionHash = txHash
+	}
+
+	c.JSON(http.StatusOK, response)
 }
